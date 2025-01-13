1667e3a1bce5ce0497c0c53995dfec9a5d527c5c
Rami Inberg <rami.inberg@gmail.com>
2025-01-13 19:10:32 +0200

    Fixed source map warning

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack(config, { dev, isServer }) {
+    if (dev && !isServer) {
+      config.devtool = 'source-map';
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
